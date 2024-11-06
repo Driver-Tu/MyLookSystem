3ec01676929e94b9b809fbcb354369a3654c6196
Driver-Tu <3109702282@qq.com>
2024-11-06 09:10:24 +0800

    first commit -v1.0.0

---
+++ src/test/java/com/zh/look/looksystem/LookSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zh.look.looksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
